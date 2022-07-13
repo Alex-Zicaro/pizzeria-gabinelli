ae03f9f79c2565671fc4e3f56dd1dc00f6084934
Alex-Zicaro <alex.zicaro@laplateforme.io>
2022-07-13 14:37:40 +0200

    css

---
+++ App/View/include/header.php
@@ -0,0 +1,108 @@
+<link rel="stylesheet" href="../CSS/include.css">
+<header class="header ">
+
+    <div class="header-bandeau ">
+        <div class="centrage display-flex-l flex-align-items-center-l flex-justify-space-between-l">
+
+            <div class="no-margin-l margin-auto" id="logo">
+                <a href="https://www.lepallavicini.fr" title="Livraison gratuite de pizza à Marseille 13">
+                    <img class="" src="templates/captain/img/interface/logo.png" alt="Livraison gratuite de pizza à Marseille 13" width="200" />
+                </a>
+            </div>
+            <label id="btMenu" for="ouvre-menu" class=" no-margin-top">
+                <span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </span>
+            </label>
+            <div class="display-flex flex-direction-column flex-align-items-end-l flex-grow-9"> 
+<!-- ici -->
+                <nav class="nav no-margin-top display-flex">
+
+                    <ul class="santa hidden">
+                        <li class="">
+                            <div class="bloc display-flex-l flex-align-items-center display-none" id="tel">
+                                <a class="font-size-big font-weight-bold" href="https://www.pizzeria-lepallavicini.fr/" rel="nofollow">
+                                    <i class="material-icons">credit_card</i>
+                                    <span class="hotliner">Paiement en ligne</span>
+                                </a>
+                            </div>
+                        </li>
+
+                        <li class="normal-padding-right-l">
+                            <div class="bloc display-flex-l flex-align-items-center display-none" id="tel">
+                                <a class="font-size-big font-weight-bold" href="tel:+33488927544" onclick="gtag('event', 'Appel Téléphonique');">
+                                    <span class="hotliner">Commande :</span>
+                                    <span class="tx">04 88 92 75 44</span>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+                <nav class="nav no-margin-top">
+                    <ul class="lutin">
+                        <li class="active">
+                            <a href="https://www.lepallavicini.fr/" title="Accueil">
+                                Accueil
+                            </a>
+                        </li>
+                        <li>
+                            <a href="la-carte-w1.html" title="La carte">
+                                La carte
+                            </a>
+                        <li>
+                            <a href="nos-pizzas-w1.html" title="Nos pizzas">
+                                Nos pizzas
+                            </a>
+                            <ul>
+                                <li>
+                                    <a href="nos-pizzas-pizzas-traditionnelles-w1.html" title="Pizzas traditionnelles">
+                                        Pizzas traditionnelles
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="nos-pizzas-pizzas-specialites-w1.html" title="Pizzas spécialités">
+                                        Pizzas spécialités
+                                </li>
+                                <li>
+                                    <a href="nos-pizzas-pizzas-sucrees-w1.html" title="Pizzas sucrées">
+                                        Pizzas sucrées
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="les-salades-w1.html" title="Les salades">
+
+                                Les salades
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="https://www.lepallavicini.fr/#toto" title="Nos formules">
+                                Nos formules
+                            </a>
+                        </li>
+                        <li>
+                            <a href="guide-local-w1.html" title="Guide local">
+
+                                Guide local
+                            </a>
+                        </li>
+
+                        <li>
+
+
+                            <a href="contact.html" title="Contact">
+                                Contact
+                            </a>
+
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+</header>
